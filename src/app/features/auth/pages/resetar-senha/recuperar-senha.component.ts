f7e6ffcf016823cf3d51bf1e865ca9cb1deb45a4
Matheus Rocha <im.rochamatheus@gmail.com>
2025-05-31 18:50:50 -0300

    update project

--- src/app/features/auth/pages/resetar-senha/recuperar-senha.component.ts
+++ src/app/features/auth/pages/resetar-senha/recuperar-senha.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-recuperar-senha',
   templateUrl: './recuperar-senha.component.html',
   styleUrls: ['./recuperar-senha.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, HttpClientModule]
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
 })
 export class RecuperarSenhaComponent {
   recuperarForm: FormGroup;
@@ -21,7 +27,7 @@ export class RecuperarSenhaComponent {
 
   criarFormularioInicial(): FormGroup {
     return this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
@@ -29,7 +35,7 @@ export class RecuperarSenhaComponent {
     return this.fb.group({
       codigo: ['', Validators.required],
       novaSenha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmarSenha: ['', [Validators.required]]
+      confirmarSenha: ['', [Validators.required]],
     });
   }
 
@@ -37,56 +43,63 @@ export class RecuperarSenhaComponent {
     if (!this.segundaEtapa && this.recuperarForm.valid) {
       const email = this.recuperarForm.value.email;
 
-      fetch("https://localhost:7080/api/Users/forgot-password", {
-        method: "POST",
+      fetch(`${environment.apiUrl}/Users/forgot-password`, {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(email)
+        body: JSON.stringify(email),
       })
-      .then(async res => {
-        if (!res.ok) {
-          const error = await res.text();
-          throw new Error(error);
-        }
-        return res.json();
-      })
-      .then(data => {
-        if (data.userID) {
-          this.userID = data.userID;
-          this.segundaEtapa = true;
-          this.recuperarForm = this.criarFormularioSegundaEtapa();
-        } else {
-          alert("Resposta inesperada do servidor.");
-        }
-      })
-      .catch((err) => {
-        console.error("Erro ao enviar:", err);
-        alert("Erro ao enviar e-mail de recuperação.");
-      });
-    }
-
-    else if (this.segundaEtapa && this.recuperarForm.valid && this.userID !== null) {
+        .then(async (res) => {
+          if (!res.ok) {
+            const error = await res.text();
+            throw new Error(error);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          if (data.userID) {
+            this.userID = data.userID;
+            this.segundaEtapa = true;
+            this.recuperarForm = this.criarFormularioSegundaEtapa();
+          } else {
+            alert('Resposta inesperada do servidor.');
+          }
+        })
+        .catch((err) => {
+          console.error('Erro ao enviar:', err);
+          alert('Erro ao enviar e-mail de recuperação.');
+        });
+    } else if (
+      this.segundaEtapa &&
+      this.recuperarForm.valid &&
+      this.userID !== null
+    ) {
       const { codigo, novaSenha, confirmarSenha } = this.recuperarForm.value;
 
       if (novaSenha !== confirmarSenha) {
-        alert("As senhas não coincidem.");
+        alert('As senhas não coincidem.');
         return;
       }
 
       const payload = {
         newPassword: novaSenha,
-        code: codigo
+        code: codigo,
       };
 
-      this.http.post<any>(`https://localhost:7080/api/Users/verify-code/${this.userID}`, payload).subscribe({
-        next: (res) => {
-          alert(res.mensagem || "Senha redefinida com sucesso.");
-        },
-        error: () => {
-          alert("Verifique o código digitado.");
-        }
-      });
+      this.http
+        .post<any>(
+          `${environment.apiUrl}/Users/verify-code/${this.userID}`,
+          payload
+        )
+        .subscribe({
+          next: (res) => {
+            alert(res.mensagem || 'Senha redefinida com sucesso.');
+          },
+          error: () => {
+            alert('Verifique o código digitado.');
+          },
+        });
     }
   }
 }
